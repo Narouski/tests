34789ae704818d072046384d35966a3aa9c613c2
Narouski <Y.Narouski@gmail.com>
2020-08-01 08:31:27 +0300

    Правки в post_new.html

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("group/<slug:slug>/", views.group_posts, name='group'),
+    path("group/<slug:slug>/", views.group_posts, name='paginator'),
     path("new/", views.new_post, name="new"),
     path("<str:username>/", views.profile, name="profile"),
     path("<str:username>/<int:post_id>/", views.post_view, name="post"),
