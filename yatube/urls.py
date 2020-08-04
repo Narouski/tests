5c58bf15a705b28c12e0a12969d52776b7c5699a
Narouski <Y.Narouski@gmail.com>
2020-08-05 00:22:35 +0300

    Дахера всего пше

--- yatube/urls.py
+++ yatube/urls.py
@@ -3,14 +3,48 @@ from django.contrib.flatpages import views
 from django.urls import include, path
 
 urlpatterns = [
-        path('admin/admin/', admin.site.urls),
-        path('about/', include('django.contrib.flatpages.urls')),
-        path('auth/', include('users.urls')),
-        path('auth/', include('django.contrib.auth.urls')),
-        path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
-        path('terms/', views.flatpage, {'url': '/terms/'}, name='terms'),
-        path('about-author/', views.flatpage, {'url': '/about-author/'}, name='about-author'),
-        path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='about-spec'),
-        path('', include('posts.urls')),
+        path(
+                'admin/admin/',
+                admin.site.urls
+        ),
+        path(
+                'about/',
+                include('django.contrib.flatpages.urls')
+        ),
+        path(
+                'auth/',
+                include('users.urls')
+        ),
+        path(
+                'auth/',
+                include('django.contrib.auth.urls')
+        ),
+        path(
+                'about-us/',
+                views.flatpage,
+                {'url': '/about-us/'},
+                name='about'
+        ),
+        path(
+                'terms/',
+                views.flatpage,
+                {'url': '/terms/'},
+                name='terms'
+        ),
+        path(
+                'about-author/',
+                views.flatpage,
+                {'url': '/about-author/'},
+                name='about-author'
+        ),
+        path(
+                'about-spec/',
+                views.flatpage,
+                {'url': '/about-spec/'},
+                name='about-spec'
+        ),
+        path(
+                '',
+                include('posts.urls')
+        ),
 ]
-
